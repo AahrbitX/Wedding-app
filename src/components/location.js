260ad08112c2403cc5183672a423d489f8ada21b
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2025-01-30 13:51:41 +0530

    Map locations update

--- src/components/location.js
+++ src/components/location.js
@@ -1,22 +1,22 @@
 'use client';
 
-
 import React from "react";
 
 const Location = () => {
   return (
-    <div className="w-full h-[400px] p-20">
-        <h1 className="text-3xl w-full flex justify-center text-black p-10 font-bold">
-          Get Locations for wedding
-        </h1>
+    <div className="w-full h-[400px] md:p-10">
+      <h1 className="text-3xl text-center text-black font-bold md:mb-6">
+        Get Locations for Wedding
+      </h1>
       <div className="w-full">
         <iframe
-          className="w-full h-[400px]"
-          frameBorder="1"
+          className="w-full h-[400px] border rounded-lg"
+          frameBorder="0"
           scrolling="no"
           marginHeight="0"
           marginWidth="0"
-          src=" https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=CSI church atoor&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=CSI church atoor&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          allowFullScreen
         ></iframe>
       </div>
     </div>
@@ -24,6 +24,3 @@ const Location = () => {
 };
 
 export default Location;
-
-
-
